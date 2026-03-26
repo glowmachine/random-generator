3e0f221ebaacaa3616ca9bbffab93607e1f7ab34
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-03-26 11:41:14 +1000

    feat: added number range setting functionality

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ class RandomApp {
 
         this.element.addEventListener('pointerdown', (e) => {
             e.preventDefault();
-            this.roll();
+            if (this.min < this.max) {
+                this.roll(this.min, this.max);
+            }
+            else {
+                this.element.textContent = '?';
+            }
         });
 
         //initial roll on page open
         this.roll();
+        this.rangeListener();
     }
 
-    roll(min = 0, max = 10) {
+    roll(min = 1, max = 10) {
         this.element.textContent =
-            Math.floor(Math.random() * 10 + 1);
+            Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    rangeListener() {
+        const min = document.querySelector('.range__min');
+        const max = document.querySelector('.range__max');
+
+        this.min = Number(min.value);
+        this.max = Number(max.value);
+
+        min.addEventListener('input', (e) => {
+            this.min = Number(min.value);
+        });
+        max.addEventListener('input', (e) => {
+            this.max = Number(max.value);
+        });
     }
 }
 
