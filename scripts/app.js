e037963ba038923f98370f71b410b2fe60ea6aa2
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-03-28 10:55:00 +1000

    refactor!: rewrite to integrate settings
    Significant edits abstract range checking to settings.js and loading
    settings from save.js, and the RandomNumberGernator logic has been
    simplified and corrected for inclusivity. This file will now handle
    different types of random generators.
    
    BREAKING CHANGE: app will not work without save.js and settings.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,61 +1,23 @@
-class RandomApp {
+import { save, loadSettings } from './save.js';
+
+class RandomNumberGenerator {
 
     constructor() {
+        loadSettings();
         this.element = document.querySelector('.random');
-        this.historyElement = document.querySelector('.history__list');
-        this.historyList = [];
-
-        this.rangeListener();
-        //initial roll on page open
-        this.roll();
+        this.roll(save.number.min, save.number.max);
 
         this.element.addEventListener('pointerdown', (e) => {
             e.preventDefault();
-            if (this.min < this.max) {
-                this.roll(this.min, this.max);
-            }
-            else {
-                // this.element.textContent = '?';
-                this.roll();
-            }
+            this.roll(save.number.min, save.number.max);
         });
     }
 
-    roll(min = 1, max = 10) {
-        this.element.textContent =
-            Math.floor(Math.random() * (max - min + 1) + min);
-        // this.addToHistory(this.element.textContent);
-        // this.renderHistory();
+    roll(min, max) {
+        this.element.textContent = save.number.inclusive ?
+            Math.floor(Math.random() * (max - min + 1)) + min :
+            Math.floor(Math.random() * (max - min - 1)) + (min + 1);
     }
-
-    rangeListener() {
-        const min = document.querySelector('.range__min');
-        const max = document.querySelector('.range__max');
-
-        if (!this.min || !this.max) return;
-        this.min = Number(min.value);
-        this.max = Number(max.value);
-
-        min.addEventListener('input', (e) => {
-            this.min = Number(min.value);
-        });
-        max.addEventListener('input', (e) => {
-            this.max = Number(max.value);
-        });
-    }
-
-    // addToHistory(newValue) {
-    //     this.historyList = [...this.historyList.slice(-4), newValue];
-    // }
-
-    // renderHistory() {
-    //     this.historyElement.innerHTML = '';
-    //     this.historyList.forEach((item) => {
-    //         const newItem = document.createElement('li');
-    //         newItem.textContent = item;
-    //         this.historyElement.appendChild(newItem);
-    //     });
-    // }
 }
 
-const randomApp = new RandomApp();
+const randomNumberGenerator = new RandomNumberGenerator();
